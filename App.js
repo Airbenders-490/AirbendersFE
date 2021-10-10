b56214fc447bb3dc41f4a72ba44d42c886eaf3ae
Stella Nguyen <stellakimnguyen@gmail.com>
2021-10-10 13:28:59 -0400

    Add tab bar navigation

--- App.js
+++ App.js
@@ -1,13 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import ClassesScreen from './src/screens/Classes.js';
+import MessagesScreen from './src/screens/Messages.js';
+import FeedScreen from './src/screens/Feed.js';
+import ScheduleScreen from './src/screens/Schedule.js';
+import ProfileScreen from './src/screens/Profile.js';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <View style={styles.container}>
+    //   <Text>Open up App.js to start working on your app!</Text>
+    //   <StatusBar style="auto" />
+    // </View>
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={{ headerShown: false }}>
+        <Tab.Screen name="Classes" component={ClassesScreen} />
+        <Tab.Screen name="Messages" component={MessagesScreen} />
+        <Tab.Screen name="Feed" component={FeedScreen} />
+        <Tab.Screen name="Schedule" component={ScheduleScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
