69fac0982f8be368483eef8c066696ca55207005
Stella Nguyen <stellakimnguyen@gmail.com>
2021-10-12 19:43:55 -0400

    Clean previous push

--- src/screens/Classes.js
+++ src/screens/Classes.js
@@ -2,7 +2,7 @@ import React, { Component, } from "react";
 import PropTypes from "prop-types";
 import styled from 'styled-components';
 import { StyleSheet, Text, View, Image } from 'react-native';
-import ScreenContainer from '../containers/ScreenContainer'
+import ScreenContainer from '../containers/ScreenContainer';
 
 class Classes extends Component {
   constructor(props) {
@@ -13,7 +13,7 @@ class Classes extends Component {
 
   render() {
     return (
-        <ScreenContainer title="Classes">
+        <ScreenContainer title="Classes" >
           <Text>Classes!</Text>
         </ScreenContainer>
     );
