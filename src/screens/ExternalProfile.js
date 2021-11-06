ab2782e410f7fbe7ac0c81af48b7d6b5d0abac16
vithura14 <vithura14@hotmail.com>
2021-11-06 18:30:37 -0400

    STUD-117: Display external user profile

---
+++ src/screens/ExternalProfile.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import theme from '../styles/theme.style.js';
+import MainContainer from '../containers/MainContainer.js';
+import { Title, Subtitle, TextBody, Caption } from '../containers/TextContainer.js';
+import ScreenContainer from '../containers/ScreenContainer.js';
+import UserProfile from '../components/profile/UserProfile.js';
+
+class Profile extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  // Write functions here
+
+  render() {
+    return (
+      <ScreenContainer>
+        <UserProfile isReadOnly={true} userID={12345} />
+      </ScreenContainer>
+    );
+  }
+}
+
+export default Profile;
--- src/screens/Schedule.js
+++ src/screens/Schedule.js
@@ -3,6 +3,11 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { StyleSheet, Text, View } from 'react-native';
 import theme from '../styles/theme.style.js';
+import { createStackNavigator } from '@react-navigation/stack';
+import { Pressable } from 'react-native';
+import ExternalProfile from './ExternalProfile';
+
+const Stack2 = createStackNavigator();
 
 class Schedule extends Component {
   constructor(props) {
@@ -11,13 +16,67 @@ class Schedule extends Component {
 
   // Write functions here
 
-  render() {
-    return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Schedule!</Text>
-        </View>
-    );
-  }
-}
+  Schedule({ navigation }) {
+    // navigate to when to meet
+       const onPressHandler = () => {
+         navigation.navigate('ExternalProfile');
+       }
+     
+       return (
+         <View >
+           <Text >
+             Schedule!
+           </Text>
+           <Pressable
+             onPress={onPressHandler}
+             style={({ pressed }) => ({ backgroundColor: pressed ? '#ddd' : '#6958' })}
+           >
+             <Text >
+             Access to External Profile 
+             </Text>
+           </Pressable>
+         </View>
+       )
+     }
+     // Write functions here
+   
+     render() {
+       return(
+       <Stack2.Navigator
+    screenOptions={{ 
+      headerStyle: {   
+       height: 90,
+   
+           backgroundColor: 'transparent',
+           elevation: 0,
+           shadowOpacity: 0,
+           borderBottomWidth: 0, } ,
+           headerTitleStyle: {
+             marginLeft:0,
+             paddingRight:0,
+               fontWeight: 'bold',
+               fontSize:25,
+             },}}
+       
+         
+       
+       >
+         <Stack2.Screen
+       
+           name="Schedule"
+           component={this.Schedule}
+           
+        
+         />
+         <Stack2.Screen
+           name="ExternalProfile"
+           component={ExternalProfile}
+     
+         />
+       </Stack2.Navigator>
+   
+       )
+     }
+   }
 
 export default Schedule;
