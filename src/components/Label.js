32cc62d93cf4bd6012a8e53a497a38122ebb06b1
Stella Nguyen <stellakimnguyen@gmail.com>
2021-11-06 21:13:54 -0400

    Update file names and codacy fixes

--- src/components/Label.js
+++ src/components/Label.js
@@ -1,9 +1,9 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import styled from 'styled-components';
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
 import StarIcon from '../assets/images/icons/star-icon.png';
 import XIcon from '../assets/images/icons/x-icon.png';
-import theme from '../styles/theme.style.js'
+import theme from '../styles/theme.style.js';
 import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 
 
@@ -14,7 +14,6 @@ class Labels extends Component {
   }
 
   render() {
-   
     return (
       <Container disabled={this.props.isReadOnly}>
         <TextContainer backgroundColor={this.props.labelColor} isReadOnly={this.props.isReadOnly}>
@@ -26,55 +25,55 @@ class Labels extends Component {
           <LabelIcon source={this.props.labelIcon} />
         </IconTag>
       </Container>
-    );
-    
-    }
+    ); 
+  }
 }
 
 // STYLED-COMPONENTS
 const Container = styled.TouchableOpacity `
   display: flex;
   flex-direction: row;
-`
+`;
+
 const TextContainer = styled.View `
   padding-horizontal: 10;
   padding-vertical: 5;
   border-top-left-radius: 100;
   border-bottom-left-radius: 100;
-  border-top-right-radius: ${props => props.isReadOnly ? "100" : "0"};;
-  border-bottom-right-radius: ${props => props.isReadOnly ? "100" : "0"};;
+  border-top-right-radius: ${props => props.isReadOnly ? '100' : '0'};
+  border-bottom-right-radius: ${props => props.isReadOnly ? '100' : '0'};
   background-color: ${props => props.backgroundColor ? props.backgroundColor : theme.COLOR_BLUE};
-`
+`;
 
 const LabelText = styled.Text`
   color: white;
   font-weight: bold;
   letter-spacing: 2;
-`
+`;
 
 const LabelIcon = styled.Image`
   tintColor: #FFFFFF;
   width: 15;
   height: 15;
-`
+`;
 
 const CustomXIcon = styled.View`
   tintColor: #FFFFFF;
   background-color: green;
   width: 15;
   height: 15;
-`
+`;
 
 const IconTag = styled.View `
   border-top-left-radius: 0;
   border-bottom-left-radius: 0;
   border-top-right-radius: 100;
   border-bottom-right-radius: 100;
-  display: ${props => props.isReadOnly ? "none" : "flex"};
+  display: ${props => props.isReadOnly ? 'none' : 'flex'};
   justify-content: center;
   padding-horizontal: 5;
   background-color: ${props => props.backgroundColor ? `${props.backgroundColor}50` : `${theme.COLOR_BLUE}50`};
-`
+`;
 
 Labels.propTypes = {
 };
