6e80315d0608de8980e53b870ffbfc9c1ed8b5d3
Stella Nguyen <stellakimnguyen@gmail.com>
2021-11-07 18:55:44 -0500

    Clean code: indents, quotes, semicolons

--- src/components/Collapse.js
+++ src/components/Collapse.js
@@ -1,9 +1,9 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { TouchableOpacity, LayoutAnimation } from 'react-native';
 import theme from '../styles/theme.style.js';
-import Icon from "react-native-vector-icons/MaterialIcons";
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 class Collapse extends Component {
   constructor(props) {
@@ -12,26 +12,27 @@ class Collapse extends Component {
       expanded : false,
     }
   }
+  
+  toggleExpand = () => {
+    LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+    this.setState({expanded : !this.state.expanded})
+  }
 
   render() {
     return (
         <CollapsibleButton onPress={()=>this.toggleExpand()} isCurrentlyTeammate={this.props.isCurrentlyTeammate}>
             <Icon name={this.state.expanded ? 'keyboard-arrow-up' : 'keyboard-arrow-down'} 
-            size={30} 
+              size={30} 
             />
         </CollapsibleButton>
     );
-  }
-
-toggleExpand=()=>{
-  LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
-  this.setState({expanded : !this.state.expanded})
   }
 }
 
 const CollapsibleButton = styled.TouchableOpacity`
   display: ${props => props.isCurrentlyTeammate ? 'flex' : 'none'};
-`
+`;
+
 Collapse.propTypes = {
     isCurrentlyTeammate: PropTypes.bool.isRequired
 };
