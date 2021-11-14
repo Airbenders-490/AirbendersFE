9ae9fc1ecb5e3ce1ee62018b02a4df29ef83c3ed
Stella Nguyen <stellakimnguyen@gmail.com>
2021-11-14 12:47:07 -0500

    Put back login page

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ import FeedScreen from './src/screens/Feed.js';
 import ScheduleScreen from './src/screens/Schedule.js';
 import ProfileScreen from './src/screens/Profile.js';
 import LoginScreen from './src/screens/Login.js';
-// import Routes from './src/Routes.js';
+import Routes from './src/Routes.js';
 
 import ClassesIconOutline from './src/assets/images/icons/graduation_cap.png';
 import MessagesIconOutline from './src/assets/images/icons/message_bubble.png';
@@ -108,7 +108,7 @@ export default class App extends React.Component {
       return (
         <NavigationContainer>
           <Tab.Navigator
-            initialRouteName='Feed'
+            initialRouteName='Login'
             screenOptions={() => ({
               tabBarActiveTintColor: '#5089E9',
               tabBarInactiveTintColor: '#CAD8F0',
@@ -117,7 +117,7 @@ export default class App extends React.Component {
               tabBarShowLabel: false,
             })}
             tabBar={(props) => <NavigationBar {...props} />}>
-            {/* <Tab.Screen name='Routes' component={Routes} /> */}
+            <Tab.Screen name='Routes' component={Routes} />
             <Tab.Screen name='Classes' component={ClassesScreen} />
             <Tab.Screen name='Messages' component={MessagesScreen} />
             <Tab.Screen name='Feed' component={FeedScreen} />
