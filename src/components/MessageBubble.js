9c21a5c75b45fba4e3b34331fb6096cb6c921b8f
nouj <55245612+nouj@users.noreply.github.com>
2021-12-06 22:18:00 -0500

    Message bubble dynamically changes according to text input

--- src/components/MessageBubble.js
+++ src/components/MessageBubble.js
@@ -2,8 +2,9 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import theme from '../styles/theme.style.js';
-import { StyleSheet, Text, View, Image, TouchableOpacity, TextInputComponent } from 'react-native';
-import TextInputContainer from '../containers/TextInputContainer'; 
+import { StyleSheet, Text, View, Image, TouchableOpacity, TextInputComponent, } from 'react-native';
+import TextInputContainer from '../containers/TextInputContainer';
+import { TextInput } from 'react-native-gesture-handler';
 
 
 class MessageBubble extends Component {
@@ -18,7 +19,12 @@ class MessageBubble extends Component {
     render() {
       return (
         <MessageBubbleContainer isAuthor={this.state.isAuthor} backgroundColor={this.props.labelColor} isSelectable={this.props.isSelectable}>
-          <TextInputContainer placeholder='Message' isConfirmed={false}>
+           <WrittenMessage
+           placeholder='Message'
+       placeholderTextColor={"#D8D8D8"}
+       multiline={true}
+     />
+          <TextInputContainer placeholder='' isConfirmed={false}>
           </TextInputContainer>
         </MessageBubbleContainer>
       ); 
@@ -26,11 +32,20 @@ class MessageBubble extends Component {
   }
   
 const MessageBubbleContainer = styled.View`
-background-color: ${props => props.isAuthor ? theme.COLOR_ORANGE : (props.backgroundColor ? props.backgroundColor :  theme.COLOR_LIGHT_GRAY)};
+background-color: ${props => props.isAuthor ? theme.COLOR_ORANGE : (props.backgroundColor ? props.backgroundColor :  theme. COLOR_LIGHT_GRAY)};
 border-radius: 12;
-padding-horizontal: 50;
-padding-vertical: 50;
+padding-vertical: 10;
+padding-horizontal: 15;
 `;
 
+const WrittenMessage = styled.TextInput`
+color:${theme.COLOR_WHITE};
+font-size: ${theme.FONT_SIZE_MEDIUM};
+font-family: ${theme.FONT_REGULAR};
+text-align: left;
+padding-vertical: ${props => props.isSelectable ? 5 : 0};
+padding-horizontal: ${props => props.isSelectable ? 5 : 0};
+top: 5;
+`;
 
 export default MessageBubble;
