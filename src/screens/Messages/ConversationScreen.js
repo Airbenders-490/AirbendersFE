0be2ad8a4dd42f1b91bce94affef8e4b96fcf820
Stella Nguyen <stellakimnguyen@gmail.com>
2022-01-30 14:36:30 -0500

    Add featured section and handle missing tab bar bug

--- src/screens/Messages/ConversationScreen.js
+++ src/screens/Messages/ConversationScreen.js
@@ -7,15 +7,67 @@ import theme from '../../styles/theme.style.js';
 import { TextBody, Title, Subtitle, Caption } from '../../containers/TextContainer.js';
 import BackIcon from '../../assets/images/icons/left-arrow.png';
 import MessageInput from '../../components/MessageInput.js';
+import FeatureButtons from '../../components/FeatureButtons.js';
+import ParticipantListItem from '../../components/ParticipantListItem.js';
 
 class ConversationScreen extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      isPinnedDisplayed: false,
+      isParticipantListDisplayed: false,
+      isCommonScheduleDisplayed: false,
+      isSearchResultDisplayed: false,
+    };
+
+    this.toggleFeaturedSection = this.toggleFeaturedSection.bind(this);
   }
 
   componentDidMount() {
     // Remove tab bar from conversation screen
-    this.props.hideTabBar();
+    this.props.hideTabBar(false);
+  }
+
+  handleBackPress(navigation) {
+    navigation.goBack();
+    // Put back tab bar on conversation exit
+    this.props.hideTabBar(true);
+  }
+
+  toggleFeaturedSection(section) {
+    // TODO: Create enums for featured sections
+    switch(section) {
+      case 'PIN': this.setState({
+        isPinnedDisplayed: true, 
+        isParticipantListDisplayed: false, 
+        isCommonScheduleDisplayed: false, 
+        isSearchResultDisplayed: false});
+        break;
+      case 'PARTICIPANTS': this.setState({
+        isPinnedDisplayed: false, 
+        isParticipantListDisplayed: true, 
+        isCommonScheduleDisplayed: false, 
+        isSearchResultDisplayed: false});
+        break;
+      case 'COMMON_SCHEDULE': this.setState({
+        isPinnedDisplayed: false, 
+        isParticipantListDisplayed: false, 
+        isCommonScheduleDisplayed: true, 
+        isSearchResultDisplayed: false});
+        break;
+      case 'SEARCH_RESULT': this.setState({
+        isPinnedDisplayed: false, 
+        isParticipantListDisplayed: false, 
+        isCommonScheduleDisplayed: false, 
+        isSearchResultDisplayed: true});
+        break;
+      default: this.setState({
+        isPinnedDisplayed: false, 
+        isParticipantListDisplayed: false, 
+        isCommonScheduleDisplayed: false, 
+        isSearchResultDisplayed: false});
+    }
   }
 
   render() {
@@ -24,14 +76,25 @@ class ConversationScreen extends Component {
     return (
         <Container>
             <Header>
-                <BackButton isVisible={this.props.isSecondaryScreen} onPress={ () => navigation.goBack() } >
+              <LHSContainer>
+                <BackButton isVisible={this.props.isSecondaryScreen} onPress={ () => this.handleBackPress(navigation) } >
                     <StyledBackIcon source={ BackIcon } />
                 </BackButton>
                 <View>
                     <Subtitle titleColor={theme.COLOR_BLACK}>Lorem Ipsum</Subtitle>
                     <TextBody captionColor={theme.COLOR_BLACK}>SOEN 490</TextBody>
                 </View>
+              </LHSContainer>
+              <FeatureButtons toggleSection={this.toggleFeaturedSection} />
             </Header>
+
+            <ExpandableSection isDisplayed={this.state.isParticipantListDisplayed}>
+              <ParticipantListItem
+                participantName={"stella nguyen"}
+                commonClass={'SOEN 490'}
+                userTeamStatus={'pending'} />
+            </ExpandableSection>
+
             <ConversationContainer>
                 <BubblesContainer />
                 <MessageInput />
@@ -42,28 +105,45 @@ class ConversationScreen extends Component {
 }
 
 //STYLED-COMPONENTS
-const Container = styled.View`
+const Container = styled.View `
   /* padding separated as the following to allow unitless values */
   padding-top: ${theme.SPACING_LARGE};
   height: 100%;
+  display: flex;
 `;
 
-const Header = styled.View`
+const Header = styled.View `
   margin-horizontal: ${theme.SPACING_MEDIUM};
   display: flex;
   flex-direction: row;
   align-items: center;
+  justify-content: space-between;
+`;
+
+const LHSContainer = styled.View `
+  display: flex;
+  flex-direction: row;
 `;
 
 const BackButton = styled.TouchableOpacity `
-    margin-right: 15;
-    display: flex;
+  margin-right: 15;
+  display: flex;
 `;
 
 const StyledBackIcon = styled.Image `
-    tint-color: ${theme.COLOR_BLACK};
-    height: 30;
-    width: 30;
+  tint-color: ${theme.COLOR_BLACK};
+  height: 30;
+  width: 30;
+`;
+
+const ExpandableSection = styled.View `
+  display: ${(props) => props.isDisplayed ? 'flex' : 'none'};
+  background: ${theme.COLOR_WHITE};
+  border-radius: ${theme.SPACING_MEDIUM};
+  padding-vertical: ${theme.SPACING_MEDIUM};
+  padding-horizontal: ${theme.SPACING_MEDIUM};
+  margin-top: ${theme.SPACING_MEDIUM};
+  margin-horizontal: ${theme.SPACING_MEDIUM};
 `;
 
 const ConversationContainer = styled.View `
