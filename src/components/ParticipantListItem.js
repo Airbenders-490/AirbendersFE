8867bfd6376fc17e36e58f35ae7941c7cf2f6a83
yasfahm <yasaman.fahm@gmail.com>
2022-03-14 15:27:35 -0400

    STUD-270: add accept/deny buttons

--- src/components/ParticipantListItem.js
+++ src/components/ParticipantListItem.js
@@ -1,6 +1,6 @@
 import React,  { Component, } from 'react';
 import PropTypes from 'prop-types';
-import { Button, View, TouchableOpacity, Text, Image, ScrollView, Pressable} from 'react-native';
+import { Button, View, TouchableOpacity, Text, Image, ScrollView, Pressable, TouchableHighlightBase} from 'react-native';
 import styled from 'styled-components';
 import theme from '../styles/theme.style.js';
 import MainContainer from '../containers/MainContainer.js';
@@ -10,6 +10,22 @@ import Label from './Label.js';
 class ParticipantListItem extends Component {
     constructor(props) {
       super(props);
+      this.state = {
+        isAdmin: true,
+    }
+    this.acceptRequest = this.acceptRequest.bind(this);
+    this.denyRequest = this.denyRequest.bind(this);
+
+  };
+
+    acceptRequest() {
+      console.log('accept participant')
+      // TODO: Add connection
+    }
+
+    denyRequest() {
+      console.log('deny participant')
+      // TODO: Add connection
     }
 
     setTeamFormationColor(status) {
@@ -40,6 +56,16 @@ class ParticipantListItem extends Component {
               <Label isReadOnly labelColor={theme.COLOR_ORANGE}>{this.props.commonClass}</Label>
               <ParticipantName>{this.props.participantName}</ParticipantName>
             </ContentLHS>
+            
+              <ButtonsContainer isAdmin={this.props.isAdmin}>
+                <TouchableOpacity onPress={this.acceptRequest} >
+                  <Label isReadOnly labelColor={theme.COLOR_GREEN}>ACCEPT</Label>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={this.denyRequest}>
+                  <Label isReadOnly labelColor={theme.COLOR_RED}>DENY</Label>
+                </TouchableOpacity>
+              </ButtonsContainer>
+            
             <TeamFormationStatus statusColor={this.setTeamFormationColor(this.props.userTeamStatus)} />
           </ContentContainer>
         </MainContainer>
@@ -64,6 +90,11 @@ const ParticipantName = styled.Text `
   font-family: ${theme.FONT_SEMIBOLD};
   font-size: ${theme.FONT_SIZE_SLIGHT_LARGE};
   text-transform: capitalize;
+`;
+
+const ButtonsContainer = styled.View `
+  display: ${props => props.isAdmin ? 'flex' : 'none'};
+  flex-direction: row;
 `;
 
 const TeamFormationStatus = styled.View `
--- src/screens/Teams.js
+++ src/screens/Teams.js
@@ -3,11 +3,12 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import theme from '../styles/theme.style.js';
-
 import ScreenContainer from '../containers/ScreenContainer';
 import { Title, Subtitle, TextBody } from '../containers/TextContainer.js';
 import MainContainer from '../containers/MainContainer.js';
 import JoinTeam from '../components/modals/JoinTeam.js';
+import ParticipantListItem from '../components/ParticipantListItem';
+
 class Teams extends Component {
   constructor(props) {
     super(props);
@@ -23,6 +24,12 @@ class Teams extends Component {
             teamName='X'
             >
           </JoinTeam>
+          <ParticipantListItem
+              participantName={"jane smith"}
+              commonClass={'SOEN 490'}
+              userTeamStatus={'pending'}
+              isAdmin={true}
+          />
         </TeamsContainer>
       </ScreenContainer>
     );
