1e3886018a482c43c74de5536c039a6dff641ce8
SanaJallouli <59178137+SanaJallouli@users.noreply.github.com>
2022-03-23 04:03:41 -0400

    Addressing comments : switch to Teams page

--- src/screens/TeamFormation/Tabs/Teams.js
+++ src/screens/TeamFormation/Tabs/Teams.js
@@ -11,9 +11,10 @@ import ListContainer from '../../../containers/ListContainer.js';
 import MainContainer from '../../../containers/MainContainer.js';
 import { Subtitle } from '../../../containers/TextContainer.js';
 import FirstConvo from '../../../data/mock/FirstConversation.json';
-import SecondConvo from '../../../data/mock/SecondConversation.json';
+import SecondConvo from '../../../data/mock/SecondConversation.json';import 
+TeamFormationTabs from "../TeamFormationTabs.js";
 import ParticipantListItem from '../../../components/ParticipantListItem';
-
+import JoinTeam from '../../../components/modals/JoinTeam.js';
 class Teams extends Component {
     constructor(props) {
       super(props);
@@ -31,22 +32,32 @@ class Teams extends Component {
             <MainContainer><Subtitle>John</Subtitle></MainContainer><Text>{'\n'}</Text>
             <MainContainer><Subtitle>John</Subtitle></MainContainer><Text>{'\n'}</Text>
             <MainContainer><Subtitle>John</Subtitle></MainContainer><Text>{'\n'}</Text>
-            <MainContainer><Subtitle>John</Subtitle></MainContainer><Text>{'\n'}</Text>
-            <MainContainer><Subtitle>John</Subtitle></MainContainer><Text>{'\n'}</Text>
-            <MainContainer><Subtitle>John</Subtitle></MainContainer><Text>{'\n'}</Text>
-            <MainContainer><Subtitle>John</Subtitle></MainContainer><Text>{'\n'}</Text>
-            <MainContainer><Subtitle>John</Subtitle></MainContainer><Text>{'\n'}</Text>
-            <MainContainer><Subtitle>John</Subtitle></MainContainer><Text>{'\n'}</Text>
-            <MainContainer><Subtitle>John</Subtitle></MainContainer><Text>{'\n'}</Text>
-            <MainContainer><Subtitle>John</Subtitle></MainContainer><Text>{'\n'}</Text>
-  
+
+
+           
+      <TeamsContainer>
+        <JoinTeam 
+          teamName='X'
+          >
+        </JoinTeam>
+      </TeamsContainer>
+     
+          <Text>{'\n'}</Text>
+          <Text>{'\n'}</Text>
+          <Text>{'\n'}</Text>
+          <Text>{'\n'}</Text>
+
           </ListContainer>
                 
            
         );
     }
 }
-
+const TeamsContainer = styled.View `
+  background-color:  ${theme.COLOR_LIGHT_GRAY};
+  height: 200; 
+  border-radius: 12;
+`;
 const ConversationItem = styled.Pressable `
     margin-bottom: ${theme.SPACING_SMALL};
 `;
