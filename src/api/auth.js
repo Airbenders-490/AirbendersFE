296d2d16cbfb756b7377b976757c2734ca446ee1
Zubair Nurie <mznurie@msn.com>
2022-04-02 22:24:00 -0400

    API/create global API object w/ module pattern

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,1 +1,44 @@
 // API calls regarding authentification
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export var AuthAPI = {
+
+    async getData(key) {
+        try {
+            return await AsyncStorage.getItem(key)
+        } catch (e) {
+            // error reading value
+            console.log("error reading value")
+            return e;
+        }
+    },
+
+    async setData(key, value) {
+        try {
+            return await AsyncStorage.setItem(key, value)
+        } catch (e) {
+            // error reading value
+            console.log("error reading value")
+            return e;
+        }
+    },
+
+    async getConfig() {
+        let token  = await this.getData('token')
+
+        return {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        }
+    },
+
+    async getUserID() {
+        return await this.getData("userID")
+    },
+
+    async getToken() {
+        return await this.getData('token')
+    }
+
+}
