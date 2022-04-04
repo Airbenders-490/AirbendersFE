42d13ee350fa9612fc75c4565f405e68953e3301
vithura14 <vithura14@hotmail.com>
2022-04-03 20:26:00 -0400

    Combine feed and chat room tests

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,58 +1,58 @@
 exports.config = {
-    host: 'localhost',
-    port: 4723,
-    runner: 'local',
-    specs: ['./test/specs/*.js'],
-    maxInstances: 1,
-    services: [
-      [
-        'appium',
-        {
-          args: {
-            relaxedSecurity: true,
-            address: '0.0.0.0',
-            port: 4723,
-            commandTimeout: '7200',
-            sessionOverride: true,
-            debugLogSpacing: true
-           },
-          command: 'appium'
-        }
-      ]
-    ],
-    capabilities: [
+  host: 'localhost',
+  port: 4723,
+  runner: 'local',
+  specs: ['./test/specs/*.js'],
+  maxInstances: 1,
+  services: [
+    [
+      'appium',
       {
-        platformName: 'Android',
-        platformVersion: '11',
-        deviceName: 'emulator-5554',
-        app: '/Users/Vithuu/Documents/Concordia/Semester 12 - Fall 2021/SOEN 490/AirbendersFE-4167bfea76e34d68942f39d73375d98e-signed.apk',
-        automationName: 'UiAutomator2'
+        args: {
+          relaxedSecurity: true,
+          address: '0.0.0.0',
+          port: 4723,
+          commandTimeout: '7200',
+          sessionOverride: true,
+          debugLogSpacing: true
+         },
+        command: 'appium'
       }
-    ],
-    logLevel: 'debug',
-    bail: 0,
-    baseUrl: 'http://localhost',
-    waitforTimeout: 10000,
-    connectionRetryTimeout: 90000,
-    connectionRetryCount: 3,
-    framework: 'mocha',
-    reporters: [
-      [
-        'allure',
-        {
-          outputDir: 'allure-results',
-          disableWebdriverStepsReporting: true,
-          disableWebdriverScreenshotsReporting: false
-        }
-      ]
-    ],
-    mochaOpts: {
-      ui: 'bdd',
-      timeout: 60000
-    },
-    afterTest: function(test, context, { error, result, duration, passed, retries }) {
-      if (!passed) {
-          browser.takeScreenshot();
-      }
+    ]
+  ],
+  capabilities: [
+    {
+      platformName: 'Android',
+      platformVersion: '11',
+      deviceName: 'emulator-5554',
+      app: '/Users/Vithuu/Documents/Concordia/Semester 12 - Fall 2021/SOEN 490/AirbendersFE-4167bfea76e34d68942f39d73375d98e-signed.apk',
+      automationName: 'UiAutomator2'
     }
-   }
+  ],
+  logLevel: 'debug',
+  bail: 0,
+  baseUrl: 'http://localhost',
+  waitforTimeout: 10000,
+  connectionRetryTimeout: 90000,
+  connectionRetryCount: 3,
+  framework: 'mocha',
+  reporters: [
+    [
+      'allure',
+      {
+        outputDir: 'allure-results',
+        disableWebdriverStepsReporting: true,
+        disableWebdriverScreenshotsReporting: false
+      }
+    ]
+  ],
+  mochaOpts: {
+    ui: 'bdd',
+    timeout: 60000
+  },
+  afterTest: function(test, context, { error, result, duration, passed, retries }) {
+    if (!passed) {
+        browser.takeScreenshot();
+    }
+  }
+ }
