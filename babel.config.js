29abadaced765409a6b258fbcd31188e73c0dbd4
carlin lee <car12lin12@gmail.com>
2022-04-20 05:07:05 -0400

    undo unnecessary changes

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,6 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    plugins:['@babel/plugin-transform-flow-strip-types'],
     presets: ['babel-preset-expo', ['@babel/preset-env', {targets: {node: 'current'}}],
     '@babel/preset-typescript',],
   };
--- src/screens/TeamFormation/Teams.js
+++ src/screens/TeamFormation/Teams.js
@@ -1,5 +1,4 @@
 import React, { Component, } from 'react';
-
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { StyleSheet, Text, View, Image } from 'react-native';
@@ -9,61 +8,18 @@ import ScreenContainer from '../../containers/ScreenContainer';
 import { Title, Subtitle, TextBody } from '../../containers/TextContainer.js';
 import MainContainer from '../../containers/MainContainer.js';
 
-
-
-// const Stack = createStackNavigator();
-
 class Teams extends Component {
   constructor(props) {
     super(props);
-
-
   }
 
   // Write functions here
 
-
-
-
   render() {
     return (
-      <TeamFormationTabs navigation={this.props.navigation}  />
+      <TeamFormationTabs navigation={this.props.navigation} />
     );
   }
 }
 
-// class TeamNavigation extends Component {
-//   constructor(props) {
-//     super(props);
-//   }
-
-//   render() {
-//     return (
-//       <Stack.Navigator
-//         screenOptions={{
-//           headerShown: false,
-//         }}
-//       >
-//         <Stack.Screen
-//           name="Teams">
-//           {(props) => <Teams
-//             navigation={this.props.navigation}
-//             route={this.props.route}
-//             hideTabBar={this.props.hideTabBar} />}
-//         </Stack.Screen>
-//         <Stack.Screen name="ExternalProfile">
-//           {(props) => <ExternalProfile />}
-//         </Stack.Screen>
-//       </Stack.Navigator>
-//     );
-//   }
-// }
-
 export default Teams;
-// export default function (props) {
-//   const navigation = useNavigation();
-//   const route = useRoute();
-
-//   return <TeamNavigation {...props} navigation={navigation} route={route} />;
-// }
-
