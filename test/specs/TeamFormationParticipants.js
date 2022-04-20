16e79a948cf93512291c0c16734a71dcde66e106
carlin lee <car12lin12@gmail.com>
2022-04-20 19:48:22 -0400

    typo

--- test/specs/TeamFormationParticipants.js
+++ test/specs/TeamFormationParticipants.js
@@ -8,7 +8,7 @@ beforeEach(() => {
  })
 
 describe('The TeamFormation Participants Recommended Teammates', () => {
-  it('GIVEN a loaded app page THEN there is a Team Chats tab', () => {
+  it('GIVEN a loaded app page THEN there is a Team Formation tab', () => {
     $('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup[2]/android.widget.ImageView').waitForDisplayed(500000)
     $('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup[2]/android.widget.ImageView').click()
     $('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup[1]/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.View/android.view.ViewGroup/android.view.View[2]/android.view.ViewGroup/android.view.ViewGroup/android.widget.TextView').waitForDisplayed(20000)
